a696d2569e7d913b00444afa7a39ef1a4cc8ad5c
SPatryk3267 <77066454+SPatryk3267@users.noreply.github.com>
2024-12-04 11:19:20 +0100

    Add files via upload

--- Powiadomienie o kalendarzu-1.0.user.js
+++ Powiadomienie o kalendarzu-1.0.user.js
@@ -8,16 +8,64 @@
 // @grant        none
 // ==/UserScript==
 
-(function () {
-    'use strict';
 
-    function checkCalendar() {
+function showWarning() {
+    var message = document.createElement("div");
+    message.id = "unclaimed-calendar-window";
+    message.classList.add("border-window", "ui-draggable", "transparent", "ui-draggable-handle");
+    message.style.cssText = `
+       position: absolute;
+       z-index: 9999;
+       display: block;
+       width: 250px;
+       height: 70px;
+       background-color: rgba(0, 0, 0, 0.8);
+       border: 1px solid rgba(161, 160, 160, 255);
+       border-radius: 5px;
+    `;
 
-        if(Engine.widgetNoticeManager.getData().includes(2)){
-            message("Uwaga! Kalendarz do odebrania!!!");
-        }
-    
+    document.body.appendChild(message);
+
+    message.style.left = `${(window.innerWidth - message.offsetWidth) / 2}px`;
+    message.style.top = `${(window.innerHeight - message.offsetHeight) / 2}px`;
+
+    var content = document.createElement("div");
+
+    content.style.cssText = `
+    font-family: Arimo, Calibri, Segoe, "Segoe UI", Optima, Arial, sans-serif;
+    line-height: 30px;
+    color: #F5F5DC;
+    overflow: hidden;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    position: relative;
+    `;
+
+    content.innerHTML = `
+        <p>Uwaga, masz nieodebrany kalendarz!!!</p>
+        <button id="close_button" style="background-color: #ff0000; color: white; padding: 5px 10px; border: 1px solid rgba(161, 160, 160, 255);
+        border-radius: 5px; cursor: pointer; bottom: 5px; left: 50%; height: 10%; ">
+            Zamknij
+        </button>
+    `;
+    message.appendChild(content);
+
+    document.getElementById("close_button").addEventListener("click", function () {
+        message.style.display = "none";
+    });
+}
+
+function run() {
+
+    if (Engine.widgetNoticeManager.getData().includes(2)) {
+        showWarning();
     }
 
-    setInterval(checkCalendar, 15000);
-})();
+}
+
+setTimeout(() => {
+    run();
+}, 5000);
